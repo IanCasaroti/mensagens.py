289bc0ed4514808b9ab0ab5b98928109cb6ae42b
Ian Casaroti Alves <67027985+IanCasaroti@users.noreply.github.com>
2024-05-20 12:43:47 -0300

    Add files via upload
    Primes

---
+++ proj10.js
@@ -0,0 +1,21 @@
+// identificar numeros primos
+
+//primos
+//composto
+
+identNPrimos(17);
+
+function showNPrimos(limit){
+    for(let n = 2; n <= limit; n++){
+        if (nPrimos(numero)) console.log(n);
+    }
+}
+
+function nPrimos(limit){
+     for(let div = 2; div < n; div++){
+            if(n % div === 0) {
+                return false;
+            }
+        }
+     return true;
+}
