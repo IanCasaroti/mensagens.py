0924bae52137d0a8277eacdc620b280ce50bf6e9
Ian Casaroti Alves <67027985+IanCasaroti@users.noreply.github.com>
2024-05-20 12:30:53 -0300

    Add files via upload
    Return Max

---
+++ proj2.js
@@ -0,0 +1,14 @@
+// função usa dois numeros e retorna o maior
+let vMaior = maior(10,8)
+console.log(vMaior);
+
+// function maior(x,y) {
+//     if(x > y)
+//         return x;
+//     else return y;
+// }
+
+//mais simples
+function maior(x,y) {
+    return x > y ? x: y;
+}
