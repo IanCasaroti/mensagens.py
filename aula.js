b9586fb83f898aac61adf83a34e4ea1a2c00fb88
Ian Casaroti Alves <67027985+IanCasaroti@users.noreply.github.com>
2024-06-05 14:14:18 -0300

    Basic studies JS

---
+++ aula.js
@@ -0,0 +1,104 @@
+//crtl+s salva tudo; ctrl+L apaga tudo; ctrl+; comenta tudo
+
+
+// let idade = 6;
+// console.log(idade);
+// let altura = 180;
+// console.log(altura);
+// camelCase
+
+// const para valores constantes e let para valores que podem ser alterados
+// const valorIngresso = 20;
+
+// console.log(valorIngresso);
+
+
+// let nome = 'Ian'; //string literal
+// let idade = 25; // number literal
+// let estaAprov = true; //boolean
+// let sobrenome = undefined; //Undefined
+// let corSelecionado = null; // redefinir um valor
+
+// let pessoa = {
+//     nome: 'Ian',
+//     idade: 25,
+//     estaAprov: true,
+//     sobrenome: 'alves'
+// };
+
+// console.log(pessoa);
+
+
+//arrays
+
+// let familia = [true,5,'ian',88]
+// console.log(familia.length)
+// console.log(familia[0])
+
+//nomear função: verbo+substantivo
+
+// let corSite = "azul";
+// function resetaCor(cor,tonalidade){
+//     corSite = cor + tonalidade;
+// }
+
+// console.log(corSite);
+// resetaCor("verde", " claro")
+// console.log(corSite);
+
+// Tipo Funções 
+// Realizar uma Tarefa, não devolve nada
+
+// function dizerNome(){
+//     console.log('ian');
+// }
+
+// dizerNome()
+
+// faz um calculo ou operação e retorna algo
+// function MultiplicarPorDois(valor){
+//     return  valor*2;
+// }
+
+// console.log(MultiplicarPorDois(5))
+
+// let resultado = MultiplicarPorDois(5);
+
+// console.log(resultado)
+
+// operadores aritimeticos
+// let salario = 100;
+//+, -, *, /, **
+
+// console.log(salario + salario);
+// console.log(salario - salario);
+// console.log(salario * salario);
+// console.log(salario / salario);
+// console.log(5 ** 5);
+
+// ++  --
+
+// let idade = 18;
+
+// console.log(--idade);
+
+// operadores atribuição
+// let valorTecgamer = 100;
+// valortecgamer = valortecgamer+valortecgamer;
+// valorTecgamer += valorTecgamer;
+
+// console.log(valorTecgamer);
+
+
+// Operadores de igualdade
+//igualdade estrita
+// console.log(1 === 1) //true
+// console.log('1' === 1) //false
+
+//igualdade solta
+// console.log(1 == 1) //true
+// console.log('1' == 1) //true
+
+
+// operadores ternarios
+// operadores bitwise
